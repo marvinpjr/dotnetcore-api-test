24e8fa7e2500e6f4e98db12b6be8f3213894e4d1
marvinpjr <marvin@marvinpalmer.com>
2018-02-15 22:04:48 -0600

    exposes some service methods on player controller

--- stats-keeper-api/Controllers/PlayerController.cs
+++ stats-keeper-api/Controllers/PlayerController.cs
@@ -1,39 +1,47 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using StatsKeeper.Api.Model;
+using StatsKeeper.Api.Services;
 
 namespace StatsKeeper.Api.Controllers
 {
     public class PlayerController : Controller
     {
+        private readonly IPlayerService playerService;
+
+        public PlayerController(IPlayerService playerService)
+        {
+            this.playerService = playerService;
+        }
+
         [HttpGet]
         public IEnumerable<Player> Players()
         {
-            return new List<Player>();
+            return playerService.GetPlayers();
         }
 
         [HttpGet("{id}")]
         public Player Player(int id)
         {
-            return new Player();
+            return playerService.GetPlayer(id);
         }
 
         [HttpPost]
         public Player Create([FromBody]Player player)
         {
-            return player;
+            return playerService.Create(player);
         }
 
         [HttpDelete("{id}")]
         public bool Delete(int id)
         {
-            return true;
+            return playerService.Delete(id);
         }
 
         [HttpPut]
         public Player Update([FromBody]Player player)
         {
-            return player;
+            return playerService.Update(player);
         }
     }
 }
