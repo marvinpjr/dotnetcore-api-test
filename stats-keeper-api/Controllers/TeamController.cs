1f1c7712a20eefbbb5c594c0ab45f2bcbef797d9
marvinpjr <marvin@marvinpalmer.com>
2018-02-15 21:44:28 -0600

    adding basicTeam service with static data

--- stats-keeper-api/Controllers/TeamController.cs
+++ stats-keeper-api/Controllers/TeamController.cs
@@ -1,39 +1,52 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using StatsKeeper.Api.Model;
+using StatsKeeper.Api.Services;
 
 namespace StatsKeeper.Api.Controllers
 {
     public class TeamController : Controller
     {
+        private readonly ITeamService teamService;
+
+        public TeamController(ITeamService teamService)
+        {
+            this.teamService = teamService;
+        }
+
+        // GET /api/team/teams
         [HttpGet]        
         public IEnumerable<Team> Teams()
         {
-            return new List<Team>();
+            return teamService.GetTeams();
         }
 
+        // GET /api/team/team/1
         [HttpGet("{id}")]
         public Team Team(int id)
         {
-            return new Team();
+            return teamService.GetTeam(id);
         }
 
+        // POST /api/team/create
         [HttpPost]
         public Team Create([FromBody]Team team)
         {
-            return team;
+            return teamService.Create(team);
         }
 
+        // DELETE /api/team/delete/1
         [HttpDelete("{id}")]
         public bool Delete(int id)
         {
-            return true;
+            return teamService.Delete(id);
         }
 
+        // PUT /api/team/update
         [HttpPut]
         public Team Update([FromBody]Team team)
         {
-            return team;
+            return teamService.Update(team);
         }
     }
 }
---
+++ stats-keeper-api/Services/TeamService.cs
@@ -0,0 +1,67 @@
+﻿using StatsKeeper.Api.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StatsKeeper.Api.Services
+{
+    public class TeamService: ITeamService
+    {
+        private static List<Team> teams;
+        private static int counter;
+
+        public TeamService()
+        {
+            teams = new List<Team>
+            {
+                new Team() {Id=1, Name="Eagles", Players=new List<Player>()},
+                new Team() {Id=2, Name="Patriots", Players=new List<Player>()}
+            };
+            counter = teams.Count;
+        }
+
+        public Team Create(Team newTeam)
+        {
+            counter++;
+            newTeam.Id = counter;
+            teams.Add(newTeam);
+            return newTeam;
+        }
+
+        public bool Delete(int id)
+        {
+            var team = teams.SingleOrDefault(t => t.Id == id);
+            if (team == null) return false;
+            int index = teams.IndexOf(team);
+            teams.RemoveAt(index);
+            return true;
+        }
+
+        public Team Update(Team team)
+        {
+            var teamToUpdate = teams.SingleOrDefault(t => t.Id == team.Id);
+            if (teamToUpdate == null) throw new KeyNotFoundException("No team with that id was found.");
+            teamToUpdate.Name = team.Name;
+            teamToUpdate.Players = team.Players;
+            return teamToUpdate;
+        }
+
+        public Team GetTeam(int id)
+        {
+            return teams.Single(t => t.Id == id);
+        }
+
+        public IEnumerable<Team> GetTeams()
+        {
+            return teams;
+        }
+    }
+
+    public interface ITeamService
+    {
+        IEnumerable<Team> GetTeams();
+        Team GetTeam(int id);
+        Team Create(Team newTeam);
+        bool Delete(int id);
+        Team Update(Team team);
+    }
+}
