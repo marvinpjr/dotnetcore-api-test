b6412e1f52295c87c7f66b96d03dfc8f3b72d231
marvinpjr <marvin@marvinpalmer.com>
2018-02-22 21:09:55 -0600

    adding sql db project

---
+++ StatKeeper.Database/Tables/Players.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Players]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [JerseyNum] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL
+)
---
+++ StatKeeper.Database/Tables/TeamPlayers.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[TeamPlayers]
+(
+	[TeamId] INT NOT NULL PRIMARY KEY, 
+    [PlayerId] INT NOT NULL
+)
