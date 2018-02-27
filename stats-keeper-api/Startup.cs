4016d89e96e198dc87d817615cb1771c74b04ec2
marvinpjr <marvin@marvinpalmer.com>
2018-02-26 21:42:42 -0600

    adding more to gitignore, trying to get logging to work

--- stats-keeper-api/Startup.cs
+++ stats-keeper-api/Startup.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using StatsKeeper.Api.Repositories;
 using StatsKeeper.Api.Services;
 using Swashbuckle.AspNetCore.Swagger;
